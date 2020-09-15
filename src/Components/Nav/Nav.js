7df000aab41d47ec851748711b8632016bc68791
Cj <carterjpilling@gmail.com>
2020-09-15 16:50:20 -0600

    toggled nav

--- src/Components/Nav/Nav.js
+++ src/Components/Nav/Nav.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import '../css/Nav.css'
-import { Link } from 'react-router-dom'
+import { withRouter, Link } from 'react-router-dom'
 import { connect } from 'react-redux'
 
 import homeLogo from '../../assets/home_logo.png'
@@ -11,21 +11,25 @@ import logoutLogo from '../../assets/shut_down.png'
 
 const Nav = props => {
   console.log(props)
-  return (
-    <div className='navbar-container'>
-      <nav className='navbar-button-container'>
-        <div className='profile-pic' src={`${props.profile_pic}`}></div>
-        <p>{props.username}</p>
-        <Link to='/dashboard'><img className='home-logo' src={homeLogo} alt='home_logo' /></Link>
-        <Link to='/new'><img className='new-logo' src={newLogo} alt='new_logo' /></Link>
-        <Link to='/'><img className='log-out-button' src={logoutLogo} alt='logout_logo' /> </Link>
-      </nav>
-    </div>
-  )
+  if (props.location.pathname !== '/') {
+    return (
+      <div className='navbar-container'>
+        <nav className='navbar-button-container'>
+          <div className='profile-pic' src={`${props.profile_pic}`}></div>
+          <p>{props.username}</p>
+          <Link to='/dashboard'><img className='home-logo' src={homeLogo} alt='home_logo' /></Link>
+          <Link to='/new'><img className='new-logo' src={newLogo} alt='new_logo' /></Link>
+          <Link to='/'><img className='log-out-button' src={logoutLogo} alt='logout_logo' /> </Link>
+        </nav>
+      </div>
+    )
+  } else {
+    return null
+  }
 }
 
 function mapStateToProps(state) {
   return state
 }
 
-export default connect(mapStateToProps)(Nav)
+export default withRouter(connect(mapStateToProps)(Nav))
