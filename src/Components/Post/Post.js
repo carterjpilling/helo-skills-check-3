080fdbec6b4c741610cc312cec8e8b07ddb824d4
Cj <carterjpilling@gmail.com>
2020-09-19 14:53:51 -0600

    finished post component

--- src/Components/Post/Post.js
+++ src/Components/Post/Post.js
@@ -28,7 +28,24 @@ class Post extends Component {
   render() {
 
     return (
-      <div className="post-container-page"> POSTS {this.state.title}</div>
+      <div className="post-container-page">
+        <div className='post-container-box'>
+          <div className='post-header'>
+            <h1>{this.state.title}</h1>
+            <div className='title-img-container'>
+              <p>By: {this.state.author_id}</p>
+              <img className='post-profile-pic' src={this.state.profile_pic} alt={this.state.profile_pic} />
+            </div>
+          </div>
+          <div className='img-content-container'>
+            {this.state.img ?
+              <img className='post-img-container' src={this.state.img} alt={this.state.img} /> :
+              <img src={'https://th.bing.com/th/id/OIP.OMoenU-E_IMw77OUoW_dcgHaEh?w=265&h=180&c=7&o=5&pid=1.7'} className='post-img-container' alt='null_picture' />}
+            <p className='post-content-container'>{this.state.content}</p>
+          </div>
+
+        </div>
+      </div>
     )
   }
 }
